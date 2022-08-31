11734a8b3dbca5c3cba9d48d8fccade76dc11bb3
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-31 12:24:01 -0600

    initial framework

---
+++ get-lit/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Body from "./components/body";
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ get-lit/src/components/body.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function About() {
+  return (
+        <div>
+            <h1> Hi </h1>
+        </div>
+  );
+}
