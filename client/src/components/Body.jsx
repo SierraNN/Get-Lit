94f7c32979200b1a22e248143b15cc001634b458
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-09-01 11:25:41 -0500

    Client/server file structure and basic express server (#2)
    * initial framework
    
    * styling
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * edit filename
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * update
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * update1
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * push
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * renamed frontend react folder to "client"
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * created server folder and installed basic dependencies
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * basic express server
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * Testing SSH signing
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    * testing ssh signature
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    
    Signed-off-by: David Taylor <94571813+codewizard-dt@users.noreply.github.com>
    Co-authored-by: Sierra Nutt <sierra.nutt@outlook.com>

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+export default function About() {
+  return (
+        <div>
+                <div className="background">
+
+            </div>
+
+            <div id="main-content">
+
+
+                <div className="paper">
+                    
+                    <h1> welcome, readers! </h1>
+                    <p>
+                        insert text here!
+                    </p>
+                </div>
+
+            </div>
+            <div id="main-navigation">
+                <ul>
+                    <li>
+                        <a href="javascript:void(0)" className="wattpad-logo">Home</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" className="entypo-layout">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" className="entypo-map">Discover</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" className="entypo-users">My Friends</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" className="entypo-archive">My Books</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)" className="entypo-mail">Share</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+  );
+}
