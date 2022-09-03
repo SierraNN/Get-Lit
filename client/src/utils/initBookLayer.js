951a0e6f2b000bc1d26ce52062460eb93a6a1805
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-09-03 11:21:57 -0500

    changing movements of the book when it comes onto the screen, installed packages needed to the public folder

--- client/src/utils/initBookLayer.js
+++ client/src/utils/initBookLayer.js
@@ -8,51 +8,51 @@ const loader = new GLTFLoader();
 
 export function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 5;
+	camera = new THREE.PerspectiveCamera( 300, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 9;
     camera.rotation.z = 1;
 
 	scene = new THREE.Scene();
-
+// Lights up the entire scene
     const light = new THREE.AmbientLight( 0xffffff, 2 );
     scene.add( light );
 
 
     loader.load("/models/scene.gltf", (gltf) => {
         let model = gltf.scene
-        model.scale.set(.45, .45, .45)
+        model.scale.set(.01, .01, .01)
 
         gsap.to(camera.position, {
-            z: 1,
+            z: 10,
             duration: 1,
             ease: "back.out(1.7)"
         })
         gsap.to(camera.rotation, {
-            z: 0,
+            z: 2,
             duration: 1
         })
 
         gsap.to(model.rotation, {
-            x: 1,
+            x: 2.5,
             duration: 1,
             delay: 1
         })
         gsap.to(model.rotation, {
-            y: Math.PI * 1.75,
+            y: Math.PI * 3,
             duration: 2,
             delay: 1
         })
         gsap.to(model.scale, {
             delay: 1,
             duration: 1,
-            x: .25,
-            y: .25,
-            z: .25
+            x: .015,
+            y: .015,
+            z: .015
         })
         gsap.to(model.position, {
             delay: 1,
             duration: 1,
-            x: .35,
+            x: .3,
             y: .3
         })
 
@@ -60,7 +60,7 @@ export function init() {
 
         scene.add(model)
     })
-
+// Renders image to the page and animates them
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animation );
