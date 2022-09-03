31cf526ade91326c38e32a67412eb50eda70c588
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-09-03 02:10:25 -0500

    Added book model, installed three js and gsap animation library

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import { init } from "./utils/initBookLayer.js";
+import { useEffect} from "react";
+
+ function App() {
+    return(
+        <div className="App">
+            Hello
+        </div>
+    )
+ }
+
+ export default App;
---
+++ client/src/utils/initBookLayer.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import gsap from 'gsap';
+
