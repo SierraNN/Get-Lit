c3363d0da76291e3aaad2ec6e66ead882cf232ff
Sierra Nutt <sierra.nutt@outlook.com>
2022-09-26 23:50:13 -0600

    adding login button and styling

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,23 +8,15 @@ function LandingPage() {
   useEffect(() => {
     init()
   }, [])
-
-  const setLanguage = useSetLanguage()
-  const getLanguage = useCurrentLanguage()
-
   return (
     <div id = "LandingPage">
       <div className="hero">
-        <h1 className="title"><T>page_title</T></h1>
-        <p className="text"><T>page_description</T></p>
-        <p>
-          <button className="btn" onClick={() => setLanguage("en")} style={{background: getLanguage() === "en" ? "#053574" : "#a7a7a7"}}>English</button>
-          <button className="btn" onClick={() => setLanguage("es")} style={{background: getLanguage() === "es" ? "#053574" : "#a7a7a7"}}>Spanish</button>
-        </p>
+        <Link to="/login">
+          <button className="btn1"><a href="#">Login to Get Lit</a></button>
+        </Link>
       </div>
       {/* content in each class has a T surrounding it to say its from tolgee */}
       <div className="choices">
-        <h1><T>choices_title</T></h1>
         <div className="choices-table">
           <div className="choices-card">
             <p className="title"><T>card1_title</T></p>
@@ -48,14 +40,7 @@ function LandingPage() {
             </Link>
             </div>
         </div>
-        
       </div>
-      <a href= "https://github.com/SierraNN/Get-Lit" target="_blank">
-      <button className="ui github button">
-      <i className="github icon"></i>
-      Github
-      </button>
-     </a>
     </div>
     
   );
