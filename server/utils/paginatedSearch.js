570babd0301e1f39330a53db28ec467c44f48308
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-09-28 17:36:08 -0500

    review rating fix (#78)
    * Review updates
    
    * handle undefined
    
    * review updates
    
    * style tweak
    
    * segment opacity and ListDetails background
    
    * ClubDetails
    
    * BookImage update
    
    * BookImage update
    
    * background1
    
    * ProfileImage, container1
    
    * CreateReview
    
    * fixed a few background / container issues, tweaked comment-list css
    
    * Reviews working but slightly glitchy--- going to implement a SearchContext
    
    * SearchContext which fixes several issues, still needs to be further tweaked
    
    * Search Query debugging
    
    * useMutationCB custom hook
    
    * Club Posts edit/delete
    
    * Updated ReviewDetails, ListDetails, ClubDetails with Add/Edit/Delete Comment functions
    
    * merge main
    
    * useSearch and useFetch hooks fully implemented for Users, Clubs, Lists, Reviews
    
    * Bug fix
    
    * fixed review rating issue

--- server/utils/paginatedSearch.js
+++ server/utils/paginatedSearch.js
@@ -1,3 +1,5 @@
+const User = require("../models/User")
+
 function paginatedSearch({ defaultType = 'name', populate = [] }) {
   return async function ({ term, type = defaultType, pageSize = 20, page = 1 }) {
     let query = {}
@@ -7,6 +9,13 @@ function paginatedSearch({ defaultType = 'name', populate = [] }) {
       if (['tags', 'comments'].includes(attr)) {
         let regex = query[attr].trim().split(' ').map((n) => `(?=.*(?:\\b${n}|${n}\\b))`).join('')
         query[attr] = { $elemMatch: { text: { $regex: regex, $options: 'i' } } }
+      } else if (attr === 'creator') {
+        console.log(attr, query[attr])
+        // query[attr]= { $lookup: { from: 'users', localField: 'creator', foreignField: '_id', as: 'user' } }
+
+        // const users = await User.find({ username: query[attr] })
+        // const ids = users.map(({ _id }) => _id)
+        // console.log({ ids })
       } else {
         let regex = query[attr].trim().split(' ').map((n) => `(?=.*(?:${n}|${n}))`).join('')
         query[attr] = { $regex: regex, $options: 'i' }
